bc78d04d0bb40dd158fd77ffbdd6875c44a9c2db
wayne.han <wayne.han@picovr.com>
2020-10-13 14:03:48 +0800

    git ignore .idea/

--- app/src/main/java/com/pvr/jetpack/databinding/BindingViewModel.kt
+++ app/src/main/java/com/pvr/jetpack/databinding/BindingViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 
 class BindingViewModel : ViewModel() {
 
-    val mNumber: MutableLiveData<Int> = MutableLiveData<Int>()
+    val mNumber: MutableLiveData<Int> = MutableLiveData<Int>(200)
 
     fun setNumber() {
         mNumber.value = 100
